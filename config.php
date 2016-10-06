45cefc7560e3d2ed0248a0888ffdb14cadae216f
digjack <244541048@qq.com>
2016-10-06 05:11:49 -0400

    1.0

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'zhuyetang',
+    'username' => 'zhuyetang',
+    'password' => '410320',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => 'http://www.zhuyetang.site:4600',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
